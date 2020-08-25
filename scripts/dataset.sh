2d691f9d0acc181072d01f7194df373d9f0032a6
isspek <ipekbrs@gmail.com>
2020-08-25 15:12:39 +0200

    add the script for extracting turkic texts

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+python -m dataset --mode extract_turkic_texts
 python -m dataset --mode split --random_seed 42
 python -m dataset --mode split --random_seed 1
 python -m dataset --mode split --random_seed 15
